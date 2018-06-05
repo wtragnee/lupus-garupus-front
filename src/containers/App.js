0ebad2af66b80fef03ec3342a3b49f9a193b1c6e
William Tragnée <w.tragnee@gmail.com>
2018-06-05 14:59:58 +0200

    Stream example

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from './Home';
 import CreateGame from './CreateGame';
+import Stream from './Stream';
 // const Nav = require('./Nav');
 
 class App extends React.Component {
@@ -11,6 +12,7 @@ class App extends React.Component {
         <div className="container">
           <Switch>
             <Route path="/create-game" component={CreateGame} />
+            <Route path="/stream" component={Stream} />
             <Route component={Home} />
           </Switch>
         </div>
